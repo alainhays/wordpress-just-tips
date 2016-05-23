ccfdfebf04590c6171c1cadd8a55a16106ab34d7
Mike Pham <mpham@nativecode.com>
2016-05-23 18:02:06 -0400

    Code formatting and added small animation on target elements.

--- app/php/plugin-video-tooltip.php
+++ app/php/plugin-video-tooltip.php
@@ -1,31 +1,31 @@
 <?php
 
 /**
- * @link              https://github/nativecode-dev/plugin-video-tooltip
- * @since             0.1.0
- * @package           nativecode-plugin-video-tooltip
- *
- * @wordpress-plugin
- * Plugin Name:       Video Tooltip
- * Plugin URI:        https://github/nativecode-dev/plugin-video-tooltip
- * Description:       Provides a shortcode that can be used to wrap an element and display a video tooltip.
- * Version:           {VERSION}
- * Author:            Mike Pham
- * Author URI:        https://www.nativecode.com
- * License:           GPL-2.0+
- * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       nativecode-plugin-video-tooltip
- */
+* @link              https://github/nativecode-dev/plugin-video-tooltip
+* @since             0.1.0
+* @package           nativecode-plugin-video-tooltip
+*
+* @wordpress-plugin
+* Plugin Name:       Video Tooltip
+* Plugin URI:        https://github/nativecode-dev/plugin-video-tooltip
+* Description:       Provides a shortcode that can be used to wrap an element and display a video tooltip.
+* Version:           {VERSION}
+* Author:            Mike Pham
+* Author URI:        https://www.nativecode.com
+* License:           GPL-2.0+
+* License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+* Text Domain:       nativecode-plugin-video-tooltip
+*/
 
 if ( ! defined( 'WPINC' ) or ! defined( 'ABSPATH' ) ) {
-  die('Nothing to do');
+    die('Nothing to do');
 }
 
 require plugin_dir_path( __FILE__ ) . 'video-tooltip.php';
 
 function video_tooltip() {
-  $plugin = new VideoTooltip();
-  $plugin->run();
+    $plugin = new VideoTooltip();
+    $plugin->run();
 }
 
 video_tooltip();
--- app/php/video-tooltip-public.php
+++ app/php/video-tooltip-public.php
@@ -1,97 +1,95 @@
 <?php
 
 class VideoTooltipPublic {
-  private $plugin_name;
-  private $version;
+    private $plugin_name;
+    private $version;
 
-  public function __construct( $plugin_name, $version ) {
-    $this->plugin_name = $plugin_name;
-    $this->version = $version;
+    public function __construct( $plugin_name, $version ) {
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
         $this->register_shortcodes();
-  }
+    }
 
-  /**
-   * @since           0.1.0
-   */
-  public function enqueue_styles() {
-    // Component styles
-    wp_enqueue_style( 'tooltipster', plugin_dir_url( __FILE__ ) . 'components/tooltipster/css/tooltipster.css', array(), $this->version, 'all' );
-    wp_enqueue_style( 'tooltipster-light', plugin_dir_url( __FILE__ ) . 'components/tooltipster/css/themes/tooltipster-light.css', array( 'tooltipster' ), $this->version, 'all' );
-    wp_enqueue_style( 'tooltipster-noir', plugin_dir_url( __FILE__ ) . 'components/tooltipster/css/themes/tooltipster-noir.css', array( 'tooltipster' ), $this->version, 'all' );
-    wp_enqueue_style( 'tooltipster-punk', plugin_dir_url( __FILE__ ) . 'components/tooltipster/css/themes/tooltipster-punk.css', array( 'tooltipster' ), $this->version, 'all' );
-    wp_enqueue_style( 'tooltipster-shadow', plugin_dir_url( __FILE__ ) . 'components/tooltipster/css/themes/tooltipster-shadow.css', array( 'tooltipster' ), $this->version, 'all' );
-    // Plugin styles
-    wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'bundle.css', array(), $this->version, 'all' );
-  }
+    /**
+    * @since           0.1.0
+    */
+    public function enqueue_styles() {
+        wp_enqueue_style( 'tooltipster', plugin_dir_url( __FILE__ ) . 'components/tooltipster/css/tooltipster.css', array(), $this->version, 'all' );
+        wp_enqueue_style( 'tooltipster-light', plugin_dir_url( __FILE__ ) . 'components/tooltipster/css/themes/tooltipster-light.css', array( 'tooltipster' ), $this->version, 'all' );
+        wp_enqueue_style( 'tooltipster-noir', plugin_dir_url( __FILE__ ) . 'components/tooltipster/css/themes/tooltipster-noir.css', array( 'tooltipster' ), $this->version, 'all' );
+        wp_enqueue_style( 'tooltipster-punk', plugin_dir_url( __FILE__ ) . 'components/tooltipster/css/themes/tooltipster-punk.css', array( 'tooltipster' ), $this->version, 'all' );
+        wp_enqueue_style( 'tooltipster-shadow', plugin_dir_url( __FILE__ ) . 'components/tooltipster/css/themes/tooltipster-shadow.css', array( 'tooltipster' ), $this->version, 'all' );
+        wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'bundle.css', array(), $this->version, 'all' );
+    }
 
-  /**
-   * @since           0.1.0
-   */
-  public function enqueue_scripts() {
-    // Component scripts
-    wp_enqueue_script( 'froogaloop2', '//f.vimeocdn.com/js/froogaloop2.min.js', array(), $this->version, false );
-    wp_enqueue_script( 'tooltipster', plugin_dir_url( __FILE__ ) . 'components/tooltipster/js/jquery.tooltipster.min.js', array( 'jquery' ), $this->version, false );
-    wp_enqueue_script( 'youtube', '//www.youtube.com/iframe_api', array(), $this->version, false );
-    // Plugin scripts
-    wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'bundle.js', array( 'jquery', 'tooltipster' ), $this->version, false );
-  }
+    /**
+    * @since           0.1.0
+    */
+    public function enqueue_scripts() {
+        // Component scripts
+        wp_enqueue_script( 'froogaloop2', '//f.vimeocdn.com/js/froogaloop2.min.js', array(), $this->version, false );
+        wp_enqueue_script( 'tooltipster', plugin_dir_url( __FILE__ ) . 'components/tooltipster/js/jquery.tooltipster.min.js', array( 'jquery' ), $this->version, false );
+        wp_enqueue_script( 'youtube', '//www.youtube.com/iframe_api', array(), $this->version, false );
+        // Plugin scripts
+        wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'bundle.js', array( 'jquery', 'tooltipster' ), $this->version, false );
+    }
 
-  /**
-   * @since           0.1.0
-   */
-  public function register_shortcodes() {
-      add_shortcode( 'video-tooltip', array( $this, 'shortcode_video_tooltip' ) );
-  }
+    /**
+    * @since           0.1.0
+    */
+    public function register_shortcodes() {
+        add_shortcode( 'video-tooltip', array( $this, 'shortcode_video_tooltip' ) );
+    }
 
-  /**
-   * @since           0.1.0
-   */
-  public function shortcode_video_tooltip( $atts, $content, $tag ) {
-      $a = shortcode_atts( array(
-          'animation' => 'swing',
-          'autohide'  => '1',
-          'autoplay'  => '1',
-          'class'     => '',
-          'color'     => '',
-          'controls'  => '0',
-          'begin'     => '0',
-          'end'       => '',
-          'id'        => '',
-          'loop'      => '0',
-          'mute'      => '0',
-          'provider'  => 'youtube',
-          'theme'     => 'tooltipster-light',
-          'video-id'  => '',
-      ), $atts );
+    /**
+    * @since           0.1.0
+    */
+    public function shortcode_video_tooltip( $atts, $content, $tag ) {
+        $a = shortcode_atts( array(
+        'animation' => 'swing',
+        'autohide'  => '1',
+        'autoplay'  => '1',
+        'class'     => '',
+        'color'     => '',
+        'controls'  => '0',
+        'begin'     => '0',
+        'end'       => '',
+        'id'        => '',
+        'loop'      => '0',
+        'mute'      => '0',
+        'provider'  => 'youtube',
+        'theme'     => 'tooltipster-light',
+        'video-id'  => '',
+        ), $atts );
 
-      $animation      = esc_attr($a['animation']);
-      $class          = esc_attr($a['class']);
-      $controls       = esc_attr($a['controls']);
-      $begin          = esc_attr($a['begin']);
-      $end            = esc_attr($a['end']);
-      $id             = esc_attr($a['id']);
-      $mute           = esc_attr($a['mute']);
-      $provider       = esc_attr($a['provider']);
-      $size           = esc_attr($a['size']);
-      $theme          = esc_attr($a['theme']);
-      $videoid        = esc_attr($a['video-id']);
+        $animation      = esc_attr($a['animation']);
+        $class          = esc_attr($a['class']);
+        $controls       = esc_attr($a['controls']);
+        $begin          = esc_attr($a['begin']);
+        $end            = esc_attr($a['end']);
+        $id             = esc_attr($a['id']);
+        $mute           = esc_attr($a['mute']);
+        $provider       = esc_attr($a['provider']);
+        $size           = esc_attr($a['size']);
+        $theme          = esc_attr($a['theme']);
+        $videoid        = esc_attr($a['video-id']);
 
-      return <<< EOT
-          <div id="$id" class="video-tooltip-shortcode $class"
-            data-provider="$provider"
-            data-video-id="$videoid"
-            data-option-animation="$animation"
-            data-option-autohide="$autohide"
-            data-option-autoplay="$autoplay"
-            data-option-begin="$begin"
-            data-option-color="$color"
-            data-option-controls="$controls"
-            data-option-end="$end"
-            data-option-loop="$loop"
-            data-option-mute="$mute"
-            data-option-theme="$theme">
-            $content
-          </div>
-EOT;
+        return <<< EOT
+        <div id="$id" class="video-tooltip-shortcode $class"
+        data-provider="$provider"
+        data-video-id="$videoid"
+        data-option-animation="$animation"
+        data-option-autohide="$autohide"
+        data-option-autoplay="$autoplay"
+        data-option-begin="$begin"
+        data-option-color="$color"
+        data-option-controls="$controls"
+        data-option-end="$end"
+        data-option-loop="$loop"
+        data-option-mute="$mute"
+        data-option-theme="$theme">
+        $content
+        </div>
+        EOT;
     }
 }
